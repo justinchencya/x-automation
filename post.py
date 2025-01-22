9a22da5574042b25fd13057b736918b66ec158bc
Justin Chen <justinchencya@Mac.lan>
2025-01-21 20:51:42 -0800

    Added create_tweet_with_media()

--- post.py
+++ post.py
@@ -18,6 +18,28 @@ client = tweepy.Client(consumer_key=api_key, consumer_secret=api_key_secret, acc
 def create_tweet(text):
     client.create_tweet(text=text)
 
+# Upload media
+auth = tweepy.OAuth1UserHandler(
+    consumer_key=api_key, consumer_secret=api_key_secret, access_token=access_token, access_token_secret=access_token_secret
+)
+
+api = tweepy.API(auth)
+
+def upload_media(media_path):
+    print(f"Uploading media: {media_path}...")
+    media = api.media_upload(media_path)
+
+    print(f"Media uploaded: {media.media_id}")
+    print(f"Media expires in: {media.expires_after_secs} seconds")
+
+    return media
+
+def create_tweet_with_media(text, media_path):
+    media = upload_media(media_path)
+    client.create_tweet(text=text, media_ids=[media.media_id_string])  
+
 if __name__ == "__main__":
     text = input("Enter the text to tweet: ")
     create_tweet(text)
+
+    # create_tweet_with_media("Hello, world!", "data/model_images/model1.png")
